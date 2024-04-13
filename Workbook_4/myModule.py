9d0e405fe18ace3e036d102250004a0afeb08694
auRIGA <baritjovel@gmail.com>
2024-04-13 20:18:14 +0800

    added workbooks 2,3,4
    added workbooks 2,3,4

---
+++ Workbook_4/myModule.py
@@ -0,0 +1,19 @@
+
+"""
+John Barit's Module
+(America Ya! - HALLO :D)
+https://www.youtube.com/watch?v=iBw-PJEg-mA
+"""
+
+magic_word = "Ambatukaaaaaam"
+
+
+def arrivalTime(distance, speed):
+    """
+    Parameter:
+        distance in km
+        speed in km/s
+    Returns: estimated arrival time in hour
+    """
+    time = float(distance) / float(speed)
+    return(time)
